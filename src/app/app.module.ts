c9a0e8d6a73cfaaaa37db622ff85689acf772d4d
Gabriel Carvalho <gabrielbdec@gmail.com>
2022-02-08 08:16:18 -0300

    refactor: app and modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthCheckModule } from './modules/health-check/health-check.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    HealthCheckModule,
+  ],
+})
+export class AppModule {}
